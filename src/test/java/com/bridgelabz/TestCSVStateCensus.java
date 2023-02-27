420dc2273eeb9e6c1fcd7f5f1d976d481760bc8c
shiwani <shiwanipaunikar1@gmail.com>
2023-02-27 09:42:25 -0800

    Added:Ability to check no. of record matches

--- src/test/java/com/bridgelabz/TestCSVStateCensus.java
+++ src/test/java/com/bridgelabz/TestCSVStateCensus.java
@@ -1,7 +1,22 @@
 package com.bridgelabz;
 
+import org.junit.jupiter.api.*;
+import org.junit.Test;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
 public class TestCSVStateCensus {
-    public static void main(String[] args) {
-        System.out.println(" Test class of Indian  State Cencus Analyser Program");
+    static CSVStateCensus csvStateCensus;
+    @Test
+    public void testReadDataFromDestinationHappy(){
+        csvStateCensus =  new CSVStateCensus();
+        try{
+            ArrayList<String> stateList = csvStateCensus.readDataFromSource();
+            Assertions.assertEquals(stateList.size(), 30);
+        } catch(Exception e){
+            fail("Should not throw exception");
+        }
     }
+
 }
+
